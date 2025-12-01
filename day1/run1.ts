91122e81d281d318fee2c9817d7f5db0ca56ff1b
Lukas Zboron <lukas.zboron@erstegroup.com>
2025-12-01 20:55:51 +0100

    day 1 improved

--- day1/run1.ts
+++ day1/run1.ts
@@ -4,20 +4,17 @@ readFile(process.argv[2], 'utf-8', (err, data) => {
     let result = 0;
     let position = 50;
     data.split('\n').forEach((line) => {
-        const value = Number(line.substring(1));
         if (!line) {
             return;
         }
+        const value = Number(line.substring(1));
         if (line.startsWith('L')) {
-            position = (100 + position - value) % 100;
-            if (position < 0) {
-                position = 100 + position;
-            }
+            position -= value;
         }
         if (line.startsWith('R')) {
-            position = (position + value) % 100;
+            position += value;
         }
-        if(position === 0) {
+        if(position % 100 === 0) {
             result++;
         }
     });
--- day1/run2.ts
+++ day1/run2.ts
@@ -4,27 +4,19 @@ readFile(process.argv[2], 'utf-8', (err, data) => {
     let result = 0;
     let position = 50;
     data.split('\n').forEach((line) => {
-        const value = Number(line.substring(1));
-        const fullTurns = Math.floor(value / 100);
         if (!line) {
             return;
         }
+        const value = Number(line.substring(1));
+        const oldTurn = Math.floor(position / 100);
         if (line.startsWith('L')) {
-            if (value > position) {
-                result += Math.floor((value) / 100);
-            }
-            if (position > 0 && value % 100 >= position) {
-                result++;
-            }
-            position = (100 + position - value) % 100;
-            if (position < 0) {
-                position = 100 + position;
-            }
+            position -= value;
         }
         if (line.startsWith('R')) {
-            result += Math.floor((position + value) / 100);
-            position = (position + value) % 100;
+            position += value;
         }
+        const newTurn = Math.floor(position / 100);
+        result += Math.abs(newTurn - oldTurn);
     });
     console.log(result);
 })
